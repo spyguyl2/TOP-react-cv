5df22749697d03d83fea3970e12b6736c090c7bc
Damon Haase <damon.haase@gmail.com>
2024-11-30 22:11:26 -0500

    Remove unused div

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 const Input = ({ text = "Change me!", type = "text" }) => (
-  <div>
+  <>
     <label htmlFor={text}>{`${text}:`}</label>
     <input type={type} name={text} id={text}></input>
-  </div>
+  </>
 );
 
 export default Input;
