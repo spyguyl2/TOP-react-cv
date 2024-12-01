86a3cc0fa7381c307d4f1bda272a01464116f4f5
Damon Haase <damon.haase@gmail.com>
2024-11-30 22:06:48 -0500

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "./styles/reset.css";
+import "./styles/Input.css";
+import Card from "./components/Card";
 import Input from "./components/Input";
 
 function App() {
   return (
-    <>
-      <section>
-        <h1>General Information</h1>
-        <Input text="First Name" />
-        <Input text="Last Name" />
-        <Input text="Email" type="email" />
-        <Input text="Phone Number" />
-      </section>
-    </>
+    <Card title="General Info">
+      <Input text="First Name" />
+    </Card>
   );
 }
 
