5edf66d2a70f1a0bc835864f6cfee31cbc58d767
Damon Haase <damon.haase@gmail.com>
2024-12-03 17:38:42 -0500

    Flesh out form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import "./styles/reset.css";
 import Card from "./components/Card";
-import Input from "./components/Input";
+import Input from "./components/LabeledInput";
+import { useState } from "react";
 
 function App() {
   return (
-    <Card title="General Info">
-      <Input text="First Name" />
-    </Card>
+    <>
+      <div id="form">
+        <button id="btn-submit">Submit</button>
+        <div className="content">
+          <Card title="General Info">
+            <Input text="First Name" />
+            <Input text="Last Name" />
+            <Input text="Phone Number" type="tel" />
+            <Input text="Email" type="email" />
+          </Card>
+          <Card title="Education">
+            <Input text="School Name"></Input>
+            <Input text="Title of Study"></Input>
+            <Input text="Date of Study" type="date"></Input>
+          </Card>
+          <Card title="Practical Experience">
+            <Input text="Company Name"></Input>
+            <Input text="Title"></Input>
+            <label htmlFor="responsibilities">
+              Describe your responsibilities:
+            </label>
+            <textarea name="responsibilities" id="responsibilities"></textarea>
+            <Input text="Start Date" type="date"></Input>
+            <Input text="End Date" type="date"></Input>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
 
